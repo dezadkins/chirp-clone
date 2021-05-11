57a5cbf25dfc6dbe30f162813edee56ba27d20a3
Dez <desmondadkins@gmail.com>
2021-05-11 11:34:03 -0400

    gitignore npmrc

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -19,12 +19,12 @@
       />
 
       <q-list>
-        <q-item clickable v-ripple>
+        <q-item clickable v-ripple to="/">
           <q-item-section avatar>
             <q-icon color="primary" name="fas fa-home" />
           </q-item-section>
 
-          <q-item-section>Home</q-item-section>
+          <q-item-section class="text-h6 text-weight-bold">Home</q-item-section>
         </q-item>
 
         <q-item clickable v-ripple>
@@ -32,7 +32,9 @@
             <q-icon color="primary" name="fas fa-hashtag" />
           </q-item-section>
 
-          <q-item-section>Explore</q-item-section>
+          <q-item-section class="text-h6 text-weight-bold"
+            >Explore</q-item-section
+          >
         </q-item>
 
         <q-item clickable v-ripple>
@@ -40,42 +42,52 @@
             <q-icon color="primary" name="far fa-bell" />
           </q-item-section>
 
-          <q-item-section>Notifications</q-item-section>
+          <q-item-section class="text-h6 text-weight-bold"
+            >Notifications</q-item-section
+          >
         </q-item>
         <q-item clickable v-ripple>
           <q-item-section avatar>
             <q-icon color="primary" name="far fa-envelope" />
           </q-item-section>
 
-          <q-item-section>Messages</q-item-section>
+          <q-item-section class="text-h6 text-weight-bold"
+            >Messages</q-item-section
+          >
         </q-item>
         <q-item clickable v-ripple>
           <q-item-section avatar>
             <q-icon color="primary" name="far fa-bookmark" />
           </q-item-section>
 
-          <q-item-section>Bookmarks</q-item-section>
+          <q-item-section class="text-h6 text-weight-bold"
+            >Bookmarks</q-item-section
+          >
         </q-item>
         <q-item clickable v-ripple>
           <q-item-section avatar>
             <q-icon color="primary" name="far fa-list-alt" />
           </q-item-section>
 
-          <q-item-section>Lists</q-item-section>
+          <q-item-section class="text-h6 text-weight-bold"
+            >Lists</q-item-section
+          >
         </q-item>
         <q-item clickable v-ripple>
           <q-item-section avatar>
             <q-icon color="primary" name="far fa-user" />
           </q-item-section>
 
-          <q-item-section>Profile</q-item-section>
+          <q-item-section class="text-h6 text-weight-bold"
+            >Profile</q-item-section
+          >
         </q-item>
         <q-item clickable v-ripple>
           <q-item-section avatar>
             <q-icon color="primary" name="fas fa-ellipsis-h" />
           </q-item-section>
 
-          <q-item-section>More</q-item-section>
+          <q-item-section class="text-h6 text-weight-bold">More</q-item-section>
         </q-item>
       </q-list>
     </q-drawer>
